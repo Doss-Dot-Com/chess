2052d8b7db20d01a391f5da26556dae7a16fbef5
malware <ddoss0531@gmail.com>
2024-10-04 20:19:22 -0600

    Phase01 Commit 5 - ChessPosition.java Correction. Pass FullGame / GameStatus Tests / Valid Move Tests

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -3,13 +3,9 @@ package chess;
 import java.util.Objects;
 
 /**
- * Represents a single square position on a chess board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
+ * Represents a specific position on the chess board.
  */
 public class ChessPosition {
-
     private final int row;
     private final int column;
 
@@ -26,13 +22,6 @@ public class ChessPosition {
         return column;
     }
 
-    /**
-     * Check if two CheckPosition objects are the same, based on row and column.
-     *
-     * @param o Compare this position to
-     * @Return True if both positions are the same row and column
-     */
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -44,13 +33,5 @@ public class ChessPosition {
     @Override
     public int hashCode() {
         return Objects.hash(row, column);
-    }
-
-    @Override
-    public String toString() {
-        return "ChessPosition{" +
-                "row=" + row +
-                ", column=" + column +
-                '}';
     }
 }
