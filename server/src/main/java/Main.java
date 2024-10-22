481f84b6406923307d5f105f56b1c136b399f57a
Malware <ddoss0531@gmail.com>
2024-10-21 20:15:56 -0600

    Phase 3 - Server Configured and Running

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,13 @@
-import chess.*;
+package server;
+
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        Server server = new Server();
+        int port = 8080;
+        server.run(port);
+
+        System.out.println("Server started on port " + port);
     }
 }
